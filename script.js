82f86fe5a886934311a3c7b50525a4f2fb909b4e
oAndin <00andherson@gmail.com>
2024-01-15 01:57:08 -0300

    updated scrpt buying stock  buttons

--- script.js
+++ script.js
@@ -1,156 +1,9 @@
-
-// // button reset ammount
-// const resetBtn = document.getElementById('reset');
-
-// function startSimu() {
-//   wallet.innerHTML = ("00.00");
-//   localStorage.setItem('wallet', wallet.innerHTML);
-//   stockPaper010.style.visibility = 'hidden';
-//   stockPaper100.style.visibility = 'hidden';
-//   stockAmount = 0;
-//   localStorage.clear();
-//   let amountStockPapers = 0;
-//   showStockAmount.innerHTML = amountStockPapers;
-// }
-
-// function resetAmount() {
-//   startSimu()
-// }
-
-// resetBtn.addEventListener('click', resetAmount);
-
-// // create the variable wallet
-// let wallet = document.getElementById('wallet');
-// // give the variable the start amount
-// wallet.innerHTML = (localStorage.getItem('wallet'));
-
-// // variable stock paper
-// const stockPaper010 = document.getElementById('stockPaper010');
-
-// // variable stock amount
-
-// let stockAmount = localStorage.getItem('stockAmount');
-// stockAmount = localStorage.getItem('stockAmount');
-
-// // variable showing how much stocks you own
-
-
-// // get html element
-// let showStockAmount = document.getElementById('showStockAmount');
-
-// // function to check if i can buy the papers
-
-// function checkStockBtnVisi() {
-//   if (wallet.innerHTML == 0.10) {
-//     stockPaper010.style.visibility = 'visible';
-//   }
-//   if (wallet.innerHTML == 1.00) {
-//     stockPaper100.style.visibility = 'visible';
-//   }
-// }
-
-// showStockAmount.innerHTML = parseInt(localStorage.getItem('stockAmount'));
-// console.log(stockAmount);
-// // save amount that im supposed to show
-
-
-// // new variable to changes on the wallet
-// let updatedWallet;
-
-// // get the clicking area
-// let clickArea = document.getElementById('clickArea')
-
-// // create the function and make it work!
-// function clickAddMoney() {
-//   let newValue = parseFloat(wallet.innerHTML) + 0.01;
-//   updatedWallet = newValue;
-//   wallet.innerHTML = updatedWallet.toFixed(2);
-//   localStorage.setItem('wallet', wallet.innerHTML);
-//   showStockAmount.innerHTML = parseInt(stockAmount);
-//   checkStockBtnVisi();
-// }
-
-// clickArea.addEventListener('click', clickAddMoney);
-
-// // get buy stock button
-
-// const buyStockBtn010 = document.getElementById("stockPaper010");
-
-// // function buy papers
-
-// const quantityBtn = document.getElementById('quantityBtn');
-
-// function handleBuyButton() {
-//   console.log("Clickei handleBuy");
-// }
-
-// quantityBtn.addEventListener('click', handleBuyButton);
-
-// function buyAndHold010() {
-//   if (wallet.innerHTML > .09) {
-//     wallet.innerHTML = (parseFloat(wallet.innerHTML) - 0.10).toFixed(2);
-//     stockAmount = stockAmount + 1;
-//     showStockAmount.innerHTML = stockAmount;
-//     localStorage.setItem('stockAmount', stockAmount);
-//   }
-//   else {
-//     alert("You dont have enough money!")
-//   }
-//   console.log(stockAmount);
-// }
-
-// buyStockBtn010.addEventListener('click', buyAndHold010);
-
-// // function dividend
-
-// setInterval(() => {
-//   if (stockAmount > 0) {
-//     let dividend = stockAmount * 0.01;
-//     wallet.innerHTML = (parseFloat(wallet.innerHTML) + parseFloat(dividend)).toFixed(2);
-//     localStorage.setItem('stockAmount', stockAmount)
-//     checkStockBtnVisi()
-//     console.log(dividend);
-//   }
-// }, 10000);
-
-// startSimu();
-
-// Time code section
-
-// start simulation/reset code section
-
-
-
-// function startSimu() {
-//   console.log("Starting simulation");
-//   timeFunction();
-//   localStorage.getItem('day') ? simulationDay = localStorage.getItem('day') : 1;
-//   simulationDay.innerHTML = simulationDay;
-//   localStorage.getItem('month') ? simulationDay = localStorage.getItem('month') : 1;
-//   simulationMonth.innerHTML = simulationDay;
-//   localStorage.getItem('year') ? simulationDay = localStorage.getItem('year') : 2024;
-//   simulationYear.innerHTML = simulationDay;
-// };
-
-// function loadGame() {
-//   simulationDay = localStorage.getItem('day');
-//   simulationDay.innerHTML = simulationDay;
-//   simulationDay = localStorage.getItem('month');
-//   simulationMonth.innerHTML = simulationDay;
-//   simulationDay = localStorage.getItem('year');
-//   simulationYear.innerHTML = simulationDay;
-// }
-
-// window.onload = loadGame();
-
-// -----------------------------------------------------------------------------------------------------------------------------
-
 // Wallet control
 
 const gameWallet = document.getElementById("walletInGame");
 let simulationMoney = localStorage.getItem('money') ? localStorage.getItem('money') : 0;
 let workedCount = 0;
-gameWallet.innerHTML = "00.00";
+gameWallet.innerHTML = "1000.00";
 
 const workArea = document.getElementById('clickArea');
 
@@ -175,26 +28,102 @@ function paymentFunction() {
 
 workArea.addEventListener('click', workFunction);
 
-
 // Stock control 
+
+const showStockQuantity = document.getElementById("showStockAmount");
+let simulationStockQuantity = 0;
+showStockQuantity.innerHTML = `Stocks: ${simulationStockQuantity}`;
 
 // stock button swtich 
 
 const stockQuantity = document.getElementById('quantityBtn');
 
 let quantity = [1, 10, 100, 1000];
-let arrayQuantity = quantity.length;
 let currentIndexOf = 0;
+let price = "$ 1.00";
 
 function handleQuantity() {
   currentIndexOf = currentIndexOf + 1;
   if (currentIndexOf == quantity.length) {
     currentIndexOf = 0;
   }
-  stockQuantity.innerHTML = `Buy stocks: ${quantity[currentIndexOf]}`;
+  stockQuantity.innerHTML = `Stock quantity: ${quantity[currentIndexOf]}`;
+  switch (currentIndexOf) {
+    case 0:
+      price = "$ 1.00"
+      break;
+    case 1:
+      price = "$ 10.00"
+      break;
+    case 2:
+      price = "$ 100.00"
+      break;
+    case 3:
+      price = "$ 1000.00"
+      break;
+    default:
+      console.log("?");
+      break;
+  }
+  buyStockBtn.innerHTML = `Buy ${quantity[currentIndexOf]} for ${price}`;
 }
-
+stockQuantity.innerHTML = `Stock quantity: ${quantity[currentIndexOf]}`
 stockQuantity.addEventListener('click', handleQuantity);
+
+const buyStockBtn = document.getElementById("buyStockBtn");
+buyStockBtn.innerHTML = `Buy ${quantity[currentIndexOf]} for ${price}`;
+buyStockBtn.addEventListener('click', buyStocks)
+const stockPrice = 1;
+
+function buyStocks() {
+  // check if user has enough money
+  switch (currentIndexOf) {
+    case 0:
+      if (parseFloat(gameWallet.innerHTML) > 0.99) {
+        simulationStockQuantity = simulationStockQuantity + 1;
+        showStockQuantity.innerHTML = `Stocks: ${simulationStockQuantity}`;
+        gameWallet.innerHTML = parseFloat(gameWallet.innerHTML - 1)
+        console.log("comprou", 1);
+      }
+      else {
+        alert("You don't have enough money!")
+      }
+      break;
+    case 1:
+      if (parseFloat(gameWallet.innerHTML) > 9.99) {
+        simulationStockQuantity = simulationStockQuantity + 10;
+        showStockQuantity.innerHTML = `Stocks: ${simulationStockQuantity}`;
+        gameWallet.innerHTML = parseFloat(gameWallet.innerHTML - 10)
+        console.log("comprou", 10);
+      }
+      else {
+        alert("You don't have enough money!")
+      }
+      break;
+    case 2:
+      if (parseFloat(gameWallet.innerHTML) > 99.99) {
+        simulationStockQuantity = simulationStockQuantity + 100;
+        showStockQuantity.innerHTML = `Stocks: ${simulationStockQuantity}`;
+        gameWallet.innerHTML = parseFloat(gameWallet.innerHTML - 100)
+        console.log("comprou", 100);
+      }
+      else {
+        alert("You don't have enough money!")
+      }
+      break;
+    case 3:
+      if (parseFloat(gameWallet.innerHTML) > 999.99) {
+        simulationStockQuantity = simulationStockQuantity + 1000;
+        showStockQuantity.innerHTML = `Stocks: ${simulationStockQuantity}`;
+        gameWallet.innerHTML = parseFloat(gameWallet.innerHTML - 1000)
+        console.log("comprou", 1000);
+      }
+      else {
+        alert("You don't have enough money!")
+      }
+      break;
+  }
+}
 
 // Game conditional control   
 
@@ -241,7 +170,11 @@ function timeFunction() {
     timeOnClock = timeOnClock + 1;
     simulationDay = parseInt(simulationDay) + 1;
     gameDay.innerHTML = simulationDay;
-    console.log("day:", simulationDay);
+    localStorage.setItem("day", simulationDay)
+    if (simulationDay == 4) {
+      simulationMoney = parseFloat(gameWallet.innerHTML);
+      console.log(simulationMoney, gameWallet.innerHTML);
+    }
     if (simulationDay == 5) {
       paymentFunction();
     }
@@ -296,4 +229,4 @@ function reset() {
     resetTimeMoney()
     :
     alert("Nothing changed!");
-};
+}
